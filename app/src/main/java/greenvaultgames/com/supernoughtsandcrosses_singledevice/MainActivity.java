2752d48ea1c6e0e483a9beb8e195f6f95a0077eb
Michael Ekins <mekins86@gmail.com>
2015-09-13 18:02:08 +0100

    Initial commit

---
+++ app/src/main/java/greenvaultgames/com/supernoughtsandcrosses_singledevice/MainActivity.java
@@ -0,0 +1,23 @@
+package greenvaultgames.com.supernoughtsandcrosses_singledevice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DrawGrid();
+    }
+
+    private void DrawGrid(){
+
+    }
+
+
+}
